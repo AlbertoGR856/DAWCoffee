ad46d7814e696794e074e67b05854c5413a3ad51
Alberto <albertogr578@gmail.com>
2020-01-15 14:42:19 +0100

    Creación clase interfaz y sus atributos

---
+++ src/main/java/com/mycompany/coffeemaker/Interfaz.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.coffeemaker;
+
+/**
+ *
+ * @author usuario
+ */
+public class Interfaz {
+    
+    private int codProducto;
+    private int cantidadComplemento;
+    private Cafetera cafetera;
+    private Administracion administracion;
+    
+}
