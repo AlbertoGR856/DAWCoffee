e7fa4505462bb78d3f6a2396f49e17ed48949aaf
Alberto <albertogr578@gmail.com>
2020-01-15 14:13:19 +0100

    Creación clase deposito y sus atributos

---
+++ src/main/java/com/mycompany/coffeemaker/Deposito.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.coffeemaker;
+
+/**
+ *
+ * @author usuario
+ */
+public class Deposito {
+    
+    private int cantidadMax;
+    private int cantidadUmbral;
+    private int cantidadActual;
+    
+//    Metodos
+//    contenido;
+//    vaciar;
+//    rellenar;
+//    servir;
+//     comprobarDeposito;
+
+  
+   
+
+    
+    
+    
+}
