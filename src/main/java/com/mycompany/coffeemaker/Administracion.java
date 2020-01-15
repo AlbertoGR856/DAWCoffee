47955f54e1bc901f55b630631d0405c6d93a3fa3
Alberto <albertogr578@gmail.com>
2020-01-15 14:44:55 +0100

    Creación clase administración y sus atributos

---
+++ src/main/java/com/mycompany/coffeemaker/Administracion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.coffeemaker;
+
+/**
+ *
+ * @author usuario
+ */
+public class Administracion {
+    
+    private int usuario;
+    private int contraseña;
+    
+}
