c0cbd6eefe2a4360b244a79d8ec0dce0aa841455
Alberto <albertogr578@gmail.com>
2020-01-15 14:29:55 +0100

    Creación clase cafetera y sus atributos

---
+++ src/main/java/com/mycompany/coffeemaker/Cafetera.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.coffeemaker;
+
+/**
+ *
+ * @author usuario
+ */
+public class Cafetera {
+    
+    private int deposito;
+    private int interfaz;
+    private int monedero;
+    private int tiposCafe;
+    
+    
+    
+    
+}
