d059e24e7897b84653cbdc71b684733b331466cc
Alberto <albertogr578@gmail.com>
2020-01-16 12:10:59 +0100

    Creación de la clase monedero y sus atributos

---
+++ src/main/java/com/mycompany/coffeemaker/Monedero.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.coffeemaker;
+
+/**
+ *
+ * @author usuario
+ */
+public class Monedero {
+    
+    private double saldoAcumulado;
+    private double saldoCliente;
+    private double precios;
+    private int numeroVentas;
+            
+    
+}
