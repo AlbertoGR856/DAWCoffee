af683dfad9c3a5862bb068d91ffcdc80e562aa84
Alberto <albertogr578@gmail.com>
2020-01-16 12:15:03 +0100

    Añade métodos getter y setter

--- src/main/java/com/mycompany/coffeemaker/Monedero.java
+++ src/main/java/com/mycompany/coffeemaker/Monedero.java
@@ -15,6 +15,38 @@ public class Monedero {
     private double saldoCliente;
     private double precios;
     private int numeroVentas;
+
+    public double getSaldoAcumulado() {
+        return saldoAcumulado;
+    }
+
+    public void setSaldoAcumulado(double saldoAcumulado) {
+        this.saldoAcumulado = saldoAcumulado;
+    }
+
+    public double getSaldoCliente() {
+        return saldoCliente;
+    }
+
+    public void setSaldoCliente(double saldoCliente) {
+        this.saldoCliente = saldoCliente;
+    }
+
+    public double getPrecios() {
+        return precios;
+    }
+
+    public void setPrecios(double precios) {
+        this.precios = precios;
+    }
+
+    public int getNumeroVentas() {
+        return numeroVentas;
+    }
+
+    public void setNumeroVentas(int numeroVentas) {
+        this.numeroVentas = numeroVentas;
+    }
             
     
 }
