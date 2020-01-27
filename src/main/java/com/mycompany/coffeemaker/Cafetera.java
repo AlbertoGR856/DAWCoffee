b2394594c890d01342664d6cb9dae275317be011
sandra <sandra.bj0308@gmail.com>
2020-01-27 14:28:29 +0100

    Creación atributos, constructor parametrizado y métodos getters y setters.

--- src/main/java/com/mycompany/coffeemaker/Cafetera.java
+++ src/main/java/com/mycompany/coffeemaker/Cafetera.java
@@ -10,14 +10,37 @@ package com.mycompany.coffeemaker;
  * @author usuario
  */
 public class Cafetera {
-    
+
     private Deposito depositoCafe, depositoAgua, depositoLeche, depositoDescafeinado;
     private Monedero monedero;
-    private TiposCafe tiposCafe;
-    
-    
-    
-    
-    
-    
+    //private TiposCafe tiposCafe;
+
+    public Cafetera(Deposito depositoCafe, Deposito depositoAgua, Deposito depositoLeche, Deposito depositoDescafeinado, Monedero monedero) {
+        this.depositoCafe = depositoCafe;
+        this.depositoAgua = depositoAgua;
+        this.depositoLeche = depositoLeche;
+        this.depositoDescafeinado = depositoDescafeinado;
+        this.monedero = monedero;
+    }
+
+    public Deposito getDepositoCafe() {
+        return depositoCafe;
+    }
+
+    public Deposito getDepositoAgua() {
+        return depositoAgua;
+    }
+
+    public Deposito getDepositoLeche() {
+        return depositoLeche;
+    }
+
+    public Deposito getDepositoDescafeinado() {
+        return depositoDescafeinado;
+    }
+
+    public Monedero getMonedero() {
+        return monedero;
+    }
+
 }
