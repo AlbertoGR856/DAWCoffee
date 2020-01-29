16e765ad95c7d45b235ad711c54de4d1bdf6f9c9
sandra <sandra.bj0308@gmail.com>
2020-01-29 13:41:37 +0100

    En el método menuAdministracion de la clase Interfaz, creo variables y hago un bucle para que el administrador pueda introducir los datos una vez más por si se equivoca.

--- src/main/java/com/mycompany/coffeemaker/Interfaz.java
+++ src/main/java/com/mycompany/coffeemaker/Interfaz.java
@@ -63,6 +63,28 @@ public class Interfaz {
 
     // Voy a hacer un menu para el administrador que será llamado desde el menú inicial.
     private void menuAdministracion() {
+        // Creo la variable solucion para que guarde el código introducido por el usuario.
+        int contraseña = 0, usuario = 0;
+        // Creo la variable intentos para que el administrador tenga más de un intento por si se equivoca introduciendo el código
+        int intentos = 0;
+        boolean confirmacion;
+        do {
+            // Mensaje de información para el usuario
+            System.out.println("A continuación, se le va a pedir el usuario y la contraseña. Va a tener 2 intentos.");
+
+            System.out.println("Introduzca el usuario.");
+            // Guardo en usuario la entrada por teclado
+            usuario = teclado.nextInt();
+
+            System.out.println("Introduzca la contraseña.");
+            // Guardo en contraseña la entrada por teclado
+            contraseña = teclado.nextInt();
+
+            confirmacion = (usuario == administracion.getUsuario() && contraseña == administracion.getContraseña()) ? false : true;
+
+            // Por si el usuario se equivoca sumo uno a la variable intentos.
+            intentos++;
+        } while (confirmacion);
        
     }
 
