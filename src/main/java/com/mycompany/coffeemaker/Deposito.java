1e11123c14c0332bd278ac148efa0eee447431c0
Sandra <sandra.bj0308@gmail.com>
2020-01-29 15:55:37 +0100

    Cambios en la clase Deposito. Añade atributo nombre, eliminación de atributos y creación de otros, cambios en el método toStrnig(). También añade comentarios.

--- src/main/java/com/mycompany/coffeemaker/Deposito.java
+++ src/main/java/com/mycompany/coffeemaker/Deposito.java
@@ -11,84 +11,73 @@ package com.mycompany.coffeemaker;
  */
 public class Deposito {
 
+    // Atributos de la clase Deposito
     private int cantidadMax;
     private int cantidadUmbral;
     private int cantidadActual;
+    private String nombre;
 
-    public Deposito(int cantidadMax, int cantidadUmbral, int cantidadActual) {
+    // Constructor parametrizado
+    public Deposito(int cantidadMax, int cantidadUmbral, int cantidadActual, String nombre) {
         this.cantidadMax = cantidadMax;
         this.cantidadUmbral = cantidadUmbral;
         this.cantidadActual = cantidadActual;
+        this.nombre = nombre;
     }
 
-    // Método que devuelve que si se vacia el deposito al llegar a la cantidadMax en caso contrario
-    // se vacia.
-    public boolean vaciar(int cantidadActual) {
-
-        if (cantidadActual > cantidadMax) {
-            return false;
-        } else {
-            cantidadActual -= cantidadActual;
-            return true;
+    // Método que rellena el deposito  una cantidad que se indica por parámetros.
+    public void rellenarCantidad(int cantidad) {
+        // Controlo que la cantidad introducida no sea negativa.
+        if (cantidad > 0) {
+            // Controlo que la cantidadActual más la cantidad que se introduce por
+            // parámetros no pueda ser mayor que la cantidadMax. Si es igual, será
+            // igual que la cantidadMax por eso tambien lo controlo.
+            if ((this.cantidadActual + cantidad) >= this.cantidadMax) {
+                this.cantidadActual = this.cantidadMax;
+            } else {
+                // Si no es mayor ni igual que la cantidadMax, lo sumo
+                this.cantidadActual += cantidad;
+            }
+        } else{
+            throw new IllegalArgumentException("La cantidad no puede ser negativa... espera, ¿cómo has conseguido ponerla negativa si no hay signo negativo en el panel númerico?");
         }
-
     }
 
-    // Método que devuelve que si se llena el deposito al llegar a la cantidadumbral en caso contrario
-    // el deposito se llenaria.
-    public boolean llenar(int cantidadActual) {
-
-        if (cantidadActual > cantidadUmbral) {
-            return false;
-        } else {
-            cantidadActual += cantidadActual;
+    // Método que indica si el contenido del depósito ha alcanzado el Umbral.
+    public boolean devolverContenido() {
+        // Controlo que la cantidadActual sea menor o igual a la cantidadUmbral
+        if (this.cantidadActual <= this.cantidadUmbral) {
+            // Si es así, devuelve true.
             return true;
+        } else {
+            // Si no, devuelve false.
+            return false;
         }
-
     }
 
-    // Método que comprueba el contenido del deposito
-    public int devolverContenido() {
-        return this.cantidadActual;
-    }
-
-    // Método que comprueba el estado del deposito de la cafetera
-    public String comprobarDeposito() {
-        return "Deposito disponible=" + cantidadActual;
-    }
-
-    // Método que sirve el cafe restandolo a la cantidad actual del deposito
-    public void servir(String contenido) {
-        this.cantidadActual -= Math.min(cantidadActual, cantidadMax);
+    // Método que rellena el depósito entero.
+    public void rellenarEntero() {
+        this.cantidadActual = this.cantidadMax;
     }
 
     public int getCantidadMax() {
         return cantidadMax;
     }
 
-    public void setCantidadMax(int cantidadMax) {
-        this.cantidadMax = cantidadMax;
-    }
-
     public int getCantidadUmbral() {
         return cantidadUmbral;
-    }
-
-    public void setCantidadUmbral(int cantidadUmbral) {
-        this.cantidadUmbral = cantidadUmbral;
     }
 
     public int getCantidadActual() {
         return cantidadActual;
     }
-
-    public void setCantidadActual(int cantidadActual) {
-        this.cantidadActual = cantidadActual;
-    }
-
+    
     @Override
     public String toString() {
-        return "Deposito{" + "cantidadMax=" + cantidadMax + ", cantidadUmbral=" + cantidadUmbral + ", cantidadActual=" + cantidadActual + '}';
+        return "El depósito " + nombre + "tiene: \n"
+                + "Cantidad umbral: " + cantidadUmbral + "gr\n"
+                + "Cantidad actual: " + cantidadActual +"gr\n"
+                + "Cantidad máxima: "+cantidadMax+"gr.";
     }
 
 }
